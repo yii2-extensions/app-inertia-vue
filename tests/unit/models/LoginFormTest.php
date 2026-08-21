9a27406b8f1eea08ad431f7f0e0dfd814f2951ab
Wilmer Arambula <terabytesoftw@gmail.com>
2026-08-21 16:15:14 -0400

    Fix Static ci.

--- tests/unit/models/LoginFormTest.php
+++ tests/unit/models/LoginFormTest.php
@@ -201,13 +201,7 @@ final class LoginFormTest extends \Codeception\Test\Unit
         $count = 0;
 
         foreach ($logger->messages as $message) {
-            if (!is_array($message)) {
-                continue;
-            }
-
-            $category = $message[2] ?? null;
-
-            if (is_string($category) && str_starts_with($category, 'yii\db\Command::query')) {
+            if (str_starts_with($message[2], 'yii\db\Command::query')) {
                 $count++;
             }
         }
